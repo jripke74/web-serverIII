aa050cc8a16b85ead9fbea4aefdd1bc62f36c84f
jripke74@gmail.com <jripke74@gmail.com>
2020-01-19 13:30:34 -0800

    update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('../src/utils/geocode')
 const forecast = require('../src/utils/forecast')
-const http = require('http');
 const port = process.env.PORT || 3000
 
 const app = express()
