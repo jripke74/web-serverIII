c9e564e0a478a82f82cea12838255e9f3bb47fd7
jripke74@gmail.com <jripke74@gmail.com>
2020-01-19 14:08:57 -0800

    add high and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/aada64998500b94d2f2e094441e903ea/${latitude},${longitude}?/lang=en`
-
+  
   request({url: url, json: true}, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service', undefined)
     } else if (body.error) {
       callback('Unable to find location.', undefined)
     } else {
-      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of snow.`)
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of snow. Hight temp: ${body.daily.data[0].temperatureHigh} and Low temp: ${body.daily.data[0].temperatureLow}`)
     }
   })
 }
